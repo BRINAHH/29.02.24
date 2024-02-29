8c15e411bebecf1d8074c3177adaeca6da1de853
SAH <143020123+BRINAHH@users.noreply.github.com>
2024-02-29 12:29:58 -0300

    Add files via upload

--- session.js
+++ session.js
@@ -7,7 +7,7 @@ function enviarSessao() { //funtion e uma declaracao de uma varialvel
     let sessao_digitada = sessionStorage.getItem(txt_login.value);//declarndo umavariavel que vai receber 
     if(sessao_digitada == null) {
         sessionStorage.setItem(txt_login.value, txt_login.value);
-        console.log("A sessao" + txt_login.value + "foi logada com sucesso.");
+        console.log("A sessao"  + txt_login.value + "foi logada com sucesso.");
         div_sessoes.innerHTML += "<br/> \r\n" + txt_login.value;//prblema nao identificado
     } else {
         console.error("Nao foi possivel enviar o login " + txt_login.value + ", pois ja ha uma sessaologada com esse nome ");
